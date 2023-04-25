85e1ce1c6fe5fd6d4660810919cabb8cc73ec0e2
digid0c <digidoc22@gmail.com>
2023-04-25 18:04:19 +0300

    Warmup assignment

---
+++ hello/src/RandomWord.java
@@ -0,0 +1,21 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+
+    public static void main(String[] args) {
+        String result = "";
+        int i = 1;
+
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if (StdRandom.bernoulli(1.0/i)) {
+                result = s;
+            }
+            i++;
+        }
+
+        StdOut.println(result);
+    }
+}
